a90702ee0d7361934f84fb71d57e4679ff6a946d
Stvim <steven.morin.fr@gmail.com>
2024-08-02 10:24:18 +0200

    Initial Commit

---
+++ Emoji Art/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Emoji Art
+//
+//  Created by Steven Morin on 02/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Emoji Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji Art
+//
+//  Created by Steven Morin on 02/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
