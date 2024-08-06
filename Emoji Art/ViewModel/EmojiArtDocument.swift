80de70794077b9ec80d8e8ab6a2e7a80bda96d70
Stvim <steven.morin.fr@gmail.com>
2024-08-06 11:36:20 +0200

    feat: autosaving and loading contents of EmojiArtDocument, with json and Data

--- Emoji Art/ViewModel/EmojiArtDocument.swift
+++ Emoji Art/ViewModel/EmojiArtDocument.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 class EmojiArtDocument: ObservableObject {
     typealias Emoji = EmojiArt.Emoji
-    @Published private var emojiArt = EmojiArt()
     
-    init() {
-        emojiArt.addEmoji("📡", at: .init(x: -200, y: -150), size: 200)
-        emojiArt.addEmoji("🛕", at: .init(x: 250, y: 100), size: 80)
+    @Published private var emojiArt = EmojiArt() {
+        didSet {
+            autosave()
+        }
     }
     
+    private let autosaveURL: URL = URL.documentsDirectory.appendingPathComponent("Autosaved.emojiart")
+    
+    private func autosave() {
+        save(to: autosaveURL)
+        print("autosaved to \(autosaveURL)")
+    }
+    
+    private func save(to url: URL) {
+        do {
+            let data = try emojiArt.json()
+            try data.write(to: url)
+        } catch let error {
+            print("EmojiArtDocument: error while saving \(error.localizedDescription)")
+        }
+    }
+
+    init() {
+        if let data = try? Data(contentsOf: autosaveURL)
+            , let autosavedEmojiArt = try? EmojiArt(json: data) {
+            emojiArt = autosavedEmojiArt
+        }
+    }
+
     var emojis: [Emoji] {
         emojiArt.emojis
     }
